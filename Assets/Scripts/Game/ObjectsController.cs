102ede572af6f7b0e0e346644445a90f10ccf6a1
Zero-Ackerman-z <delia.huayas@tecsup.edu.pe>
2024-05-05 18:03:04 -0500

    Input system y Mecanica(2)
    -Input system
    -Scriptable Object

--- Assets/Scripts/Game/ObjectsController.cs
+++ Assets/Scripts/Game/ObjectsController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CandyController : MonoBehaviour
+public class ObjectsController : MonoBehaviour
 {
     public int frame;
     public int lifeChanges;
@@ -11,7 +11,7 @@ public class CandyController : MonoBehaviour
     {
         if (transform.position.x <= -Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).x)
         {
-            CandyGenerator.instance.ManageCandy(this);
+            ObjectGenerator.instance.ManageObjects(this);
         }
     }
 }
